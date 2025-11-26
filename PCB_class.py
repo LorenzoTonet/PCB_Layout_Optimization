b82a5b43aab8a6e7b0276b4eeb7079d198c9a89a
Lorenzo <lorenzo.tonet.03@gmail.com>
2025-11-26 19:00:54 +0100

    added PCB_class

---
+++ PCB_class.py
@@ -0,0 +1,19 @@
+from typing import List, Tuple, Dict, Optional
+vec2D = Tuple[float, float]
+
+
+class PCB:
+    def __init__(self):
+        pass 
+
+    def random_placement(self):
+        pass
+
+    def calculate_area(self):
+        pass
+
+    def calculate_max_temp(self):
+        pass
+    
+    def resolve_conflicts(self):
+        pass
